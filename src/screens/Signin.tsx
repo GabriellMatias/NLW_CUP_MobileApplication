09a6c559dd33a47a39e489ab7057319d989b4771
GabriellMatias <gabriel.goiasat@gmail.com>
2022-11-01 13:52:40 -0300

    First Commit, updating files

---
+++ src/screens/Signin.tsx
@@ -0,0 +1,14 @@
+import {Text, Center} from 'native-base'
+
+
+export function Signin(){
+  return(
+    <Center flex={1} bgColor="gray.700">
+
+        <Text color="white">
+          Signin in
+          </Text>
+      </Center>
+
+  )
+}
